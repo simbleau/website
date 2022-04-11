d430c206ac9196fd6f58c6e33989b726ac0fe1e3
Spencer Imbleau <spencer@imbleau.com>
2022-04-11 10:58:02 -0400

    Added construction page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use yew::prelude::*;
+
+struct ConstructionMessage {
+    expected_end: String,
+    title: String,
+    message: String,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let construction = ConstructionMessage {
+        expected_end: "May 2022".to_string(),
+        title: "Under Construction".to_string(),
+        message: "Pardon our dust!".to_string(),
+    };
+
+    html! {
+        <>
+            <div align="center">
+                <h1>{ construction.title }</h1>
+                <h3>{ construction.message }</h3>
+                <p>{ format!("Expected completion: {}", construction.expected_end) }</p>
+            </div>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
