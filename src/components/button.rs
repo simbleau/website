76f86bff745976eb963b2983fc6aab01f80c200e
Spencer Imbleau <spencer@imbleau.com>
2022-07-02 10:11:15 -0400

    Button initial commit

---
+++ src/components/button.rs
@@ -0,0 +1,35 @@
+use stylist::css;
+use yew::prelude::*;
+
+use crate::style::use_theme;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub onclick: Callback<MouseEvent>,
+    pub inner_text: fn() -> Html,
+}
+
+#[function_component(Button)]
+pub fn button(props: &Props) -> Html {
+    let theme = use_theme();
+
+    let style = css!(
+        r#"
+            width: 2em;
+            height: 2em;
+            display:block;
+            border: 0;
+            border-radius: 50%;
+            background-color: ${bg};
+            color: ${fg};
+            padding: 10px;
+            cursor:pointer;
+        "#,
+        bg = theme.fg1.to_css(),
+        fg = theme.bg1.to_css(),
+    );
+
+    html! {
+        <button class={style} onclick={&props.onclick}>{ (props.inner_text)() }</button>
+    }
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,2 +1,5 @@
 mod theme_switcher;
 pub use theme_switcher::ThemeSwitcher;
+
+mod button;
+pub use button::Button;
