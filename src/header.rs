200fe460948cf31d7aabad4fb6131e171037014f
Spencer Imbleau <spencer@imbleau.com>
2022-07-01 22:52:22 -0400

    Organized padding for header

--- src/header.rs
+++ src/header.rs
@@ -2,15 +2,18 @@ use crate::navigation::Navigation;
 use stylist::yew::styled_component;
 use yew::prelude::*;
 
+pub const HEADER_PADDING: &str = "5px";
+
 #[styled_component(Header)]
 pub fn header() -> Html {
     let header_css = css!(
         r#"
             width: 100%;
             background-color: #dadada;
-            padding-top: 5px;
-            padding-bottom: 5px;
-        "#
+            padding-top: ${header_padding};
+            padding-bottom: ${header_padding};
+        "#,
+        header_padding = HEADER_PADDING,
     );
 
     html! {
