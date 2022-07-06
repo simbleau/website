8a8ebe2fc75ac5240022b96be4690599404227df
Spencer Imbleau <spencer@imbleau.com>
2022-07-06 18:33:02 -0400

    Finished light theme colors

--- src/style/themes.rs
+++ src/style/themes.rs
@@ -5,12 +5,42 @@ use crate::style::Color;
 use crate::style::Theme;
 
 pub static LIGHT_THEME: Lazy<Theme> = Lazy::new(|| Theme {
-    fg1: BLACK,
-    fg2: BLACK,
-    bg1: WHITE,
-    bg2: WHITE,
-    ac1: BLACK,
-    ac2: BLACK,
+    fg1: Color {
+        red: 0x44,
+        green: 0x44,
+        blue: 0x44,
+        alpha: 0xff,
+    },
+    fg2: Color {
+        red: 0x33,
+        green: 0x33,
+        blue: 0x33,
+        alpha: 0xff,
+    },
+    bg1: Color {
+        red: 0xff,
+        green: 0xff,
+        blue: 0xff,
+        alpha: 0xff,
+    },
+    bg2: Color {
+        red: 0xf1,
+        green: 0xf1,
+        blue: 0xf1,
+        alpha: 0xff,
+    },
+    ac1: Color {
+        red: 0x00,
+        green: 0x06,
+        blue: 0xee,
+        alpha: 0xff,
+    },
+    ac2: Color {
+        red: 0x00,
+        green: 0x99,
+        blue: 0xff,
+        alpha: 0xff,
+    },
 });
 
 pub static DARK_THEME: Lazy<Theme> = Lazy::new(|| Theme {
