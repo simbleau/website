824bcb523e2a418ac5f7bc1cfef2739ced3ca6d0
Spencer Imbleau <spencer@imbleau.com>
2022-07-08 07:44:59 -0400

    Added calendar icon to nav. Bug exists with hover.

--- src/navigation.rs
+++ src/navigation.rs
@@ -1,60 +1,73 @@
+use std::fmt;
+
 use stylist::yew::styled_component;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-use crate::router::Route;
+use crate::style::icons::{Icon, IconMask};
+use crate::{router::Route, style::use_theme};
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub enum NavEntry {
-    Local(fn() -> Html, Route),
-    External(fn() -> Html, &'static str),
+    Local(Html, Route),
+    External(Html, &'static str),
 }
-
-pub const SEPARATOR: fn() -> Html = || html! { {" "} };
-pub const NAV_LINKS: [NavEntry; 6] = [
-    NavEntry::Local(
-        || html! {<><i class="i-profile" />{" Home"}</>},
-        Route::Home,
-    ),
-    NavEntry::External(
-        || html! {<><i class="i-pencil" />{" Blog"}</>},
-        "https://spencer.imbleau.com/blog/",
-    ),
-    NavEntry::Local(
-        || html! {<><i class="i-info" />{" Résumé"}</>},
-        Route::Resume,
-    ),
-    NavEntry::Local(
-        || html! {<><i class="i-bookmark" />{" Contributions"}</>},
-        Route::Contributions,
-    ),
-    NavEntry::Local(
-        || html! {<><i class="i-donate" />{" Sponsor"}</>},
-        Route::Sponsor,
-    ),
-    NavEntry::Local(
-        || html! {<><i class="i-idcard" />{" Contact"}</>},
-        Route::Contact,
-    ),
-];
 
 #[styled_component(Navigation)]
 pub fn navigation() -> Html {
+    let theme = use_theme();
+
+    let separator = html! { {" "} };
+    let nav_links = [
+        NavEntry::Local(
+            html! {<><i class="i-profile" />{" Home"}</>},
+            Route::Home,
+        ),
+        NavEntry::External(
+            html! {<><i class="i-pencil" />{" Blog"}</>},
+            "https://spencer.imbleau.com/blog/",
+        ),
+        NavEntry::Local(
+            html! {<><i class="i-info" />{" Résumé"}</>},
+            Route::Resume,
+        ),
+        NavEntry::Local(
+            html! {<><Icon mask={IconMask::Calendar} fill={theme.ac1} />{" Contributions"}</>},
+            Route::Contributions,
+        ),
+        NavEntry::Local(
+            html! {<><i class="i-donate" />{" Sponsor"}</>},
+            Route::Sponsor,
+        ),
+        NavEntry::Local(
+            html! {<><i class="i-idcard" />{" Contact"}</>},
+            Route::Contact,
+        ),
+    ];
+
     html! {
         <nav>
             <ul>
             {
-                NAV_LINKS.iter().map(|n: &NavEntry| {
+                nav_links.iter().map(|n: &NavEntry| {
                     match n {
                         NavEntry::Local(display, route) => html!{
-                            <li><Link<Route> to={ *route }>{ display() }</Link<Route>></li>
+                            <li>
+                                <Link<Route> to={ *route }>
+                                    { display.clone() }
+                                </Link<Route>>
+                            </li>
                         },
                         NavEntry::External(display, url) => html!{
-                            <li><a href={ *url }>{ display() }</a></li>
+                            <li>
+                                <a href={ *url }>
+                                    { display.clone() }
+                                </a>
+                            </li>
                         },
                     }
                 })
-                .intersperse(SEPARATOR())
+                .intersperse(separator)
                 .collect::<Html>()
             }
             </ul>
