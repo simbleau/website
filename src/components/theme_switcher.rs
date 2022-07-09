fc01f99f33204f7b1cfc5fe11b48cc21589b6ba0
Spencer Imbleau <spencer@imbleau.com>
2022-07-09 12:05:12 -0400

    Added animation to icon transition

--- src/components/theme_switcher.rs
+++ src/components/theme_switcher.rs
@@ -9,6 +9,7 @@ use crate::style::{use_theme, ThemeChoice};
 
 const ICON_SIZE: &str = "1.6em";
 const ICON_PADDING: &str = ".5em";
+const TRANSITION_SPEED: &str = "0.5s";
 
 struct SwitcherColors {
     icon: Color,
@@ -46,16 +47,18 @@ pub fn theme_switcher() -> Html {
                 height: calc(${size} + 2 * ${padding});
                 text-align:center;
 
-                transition: background-color 0.5s;
+                transition: ${speed};
                 background-color: ${bg};
             }
 
             & * {
+                transition: ${speed};
                 pointer-events: none;
             }
         "#,
         size = ICON_SIZE,
         padding = ICON_PADDING,
+        speed = TRANSITION_SPEED,
         bg = switcher_colors.circle,
     );
 
