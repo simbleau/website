8402a72d912012fc598b55d2f52e72ec07c29b67
Spencer C. Imbleau <spencer@imbleau.com>
2023-01-06 22:51:15 -0500

    Fixed dark theme colors

--- src/style/themes/dark_theme.rs
+++ src/style/themes/dark_theme.rs
@@ -3,11 +3,11 @@ use cssugar::prelude::*;
 use once_cell::sync::Lazy;
 
 pub static DARK_THEME: Lazy<BrandTheme> = Lazy::new(|| BrandTheme {
-    color: Color::from_rgb(0xf8, 0xf9, 0xfa),
-    background_color: Color::from_rgb(0x21, 0x25, 0x29),
-    link: Color::from_rgb(0x00, 0x33, 0xdd),
-    link_hover: Color::from_rgb(0x0d, 0x6e, 0xfd),
-    header_color: Color::from_rgb(0x21, 0x25, 0x29),
+    color: Color::from_rgb(0xcc, 0xcc, 0xcc),
+    background_color: Color::from_rgb(0x22, 0x22, 0x22),
+    link: Color::from_rgb(0x00, 0xaa, 0xff),
+    link_hover: Color::from_rgb(0x00, 0xcf, 0xff),
+    header_color: Color::from_rgb(0xcc, 0xcc, 0xcc),
     header_font: "Roboto",
     body_font: "Roboto",
     mono_font: "Roboto",
