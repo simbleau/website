d2550e38baacf1adeb803fb58277acddc271eeb0
Spencer C. Imbleau <spencer@imbleau.com>
2023-01-08 11:47:24 -0500

    pfp incompatability with mobile and safari

--- src/components/pfp.rs
+++ src/components/pfp.rs
@@ -13,32 +13,23 @@ pub fn view(props: &ProfilePictureProps) -> Html {
     let theme = use_theme::<ThemeChoice>();
 
     let img_css = css! {
-        display: block;
-        border-radius: 50%;
-        width: 300px;
-        max-width: 600px;
-        height: 300px;
-        max-height: 600px;
-        object-fit: scale-down;
-    };
-
-    let ctr_css = css! {
         r#"
-        & {
-            background:
-                linear-gradient( white, white ) padding-box,
-                linear-gradient(to bottom, ${color}, ${shadow_normal} ) border-box;
-            outline: solid 2px ${color};
+            border-radius: 50%;
+            width: 300px;
+            max-width: 600px;
+            height: 300px;
+            max-height: 600px;
+            object-fit: scale-down;
+
+            /* Outline */
+            border: solid 3px ${color};
             box-shadow: 0 0 15px ${shadow_normal};
-
-            transition-property: outline, background, box-shadow, width, height;
+            transition-property: border, background, box-shadow, width, height;
             transition-duration: 0.5s;
-        }
-
-        &:hover {
-            outline: solid 6px ${shadow_normal};
-            box-shadow: 0 0 25px ${shadow_hover};
-        }
+            &:hover {
+                border: solid 3px ${shadow_normal};
+                box-shadow: 0 0 25px ${shadow_hover};
+            }
         "#,
         color = theme.color,
         shadow_normal = theme.color.alpha(0.2),
@@ -46,20 +37,12 @@ pub fn view(props: &ProfilePictureProps) -> Html {
     };
 
     html! {
-        <div
-            class={
-                classes!(img_css.clone(), ctr_css)
-            }
-            style="position: relative;"
-        >
             <img
-                id="fg"
                 alt="Spencer C. Imbleau"
                 width="300px"
                 height="300px"
                 class={img_css}
                 src={ props.src.clone() }
             />
-        </div>
     }
 }
