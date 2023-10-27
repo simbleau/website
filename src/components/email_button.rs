ec2a9a31c52016e9cd47757e1eb48f2ef3ef7d6d
Spencer C. Imbleau <spencer@imbleau.com>
2023-10-27 11:56:43 -0400

    feat: email copy to clipboard

--- src/components/email_button.rs
+++ src/components/email_button.rs
@@ -1,3 +1,4 @@
+use crate::util::lighten;
 use crate::{
     components::{Icon, IconMask},
     style::themes::ThemeChoice,
@@ -40,6 +41,19 @@ pub fn EmailButton(props: &EmailButtonProps) -> Html {
             position: relative;
         }
 
+        & > #wrapper {
+            position: absolute;
+            left: 75px;
+            top: -10px;
+            border: 1px solid green;
+            text-align: center;
+            align-items: center;
+            display: inline-flex;
+        }
+        & > #wrapper > *[data-aui-id="help"] {
+            margin: 10px;
+        }
+
         &::before {
             content: attr(data-tooltip);
             width: 200px;
@@ -64,7 +78,9 @@ pub fn EmailButton(props: &EmailButtonProps) -> Html {
         box-sizing: border-box;
     };
 
-    let email_css = css! {
+    let email_input_css = css! {
+        background-color: ${theme.background_color};
+        color: ${theme.color};
         border-top-left-radius: ${BORDER_RADIUS};
         border-bottom-left-radius: ${BORDER_RADIUS};
         width: ${format!("calc(100% - {})", COPY_BUTTON_WIDTH)};
@@ -77,23 +93,34 @@ pub fn EmailButton(props: &EmailButtonProps) -> Html {
         border-color: ${theme.color};
         box-sizing: border-box;
         vertical-align: baseline;
+        text-align: center;
         margin: 0;
     };
 
-    let copy_css = css! {
+    let copy_button_css = css! {
+        background-color: ${theme.background_color};
+        color: ${theme.color};
         border-top-right-radius: ${BORDER_RADIUS};
         border-bottom-right-radius: ${BORDER_RADIUS};
         width: ${COPY_BUTTON_WIDTH};
         height: 100%;
-        background_color: ${theme.color.with_a(20)};
         border-width: 3px;
         border-style: solid;
         border-color: ${theme.color};
         box-sizing: border-box;
         margin: 0;
+
+        &:hover {
+            background-color: ${lighten(&theme.background_color, 1.2)};
+            color: ${lighten(&theme.color, 1.2)};
+        }
+
+        &:hover *[data-aui-id="copy"] {
+            background: ${lighten(&theme.color, 1.2)};
+        }
     };
 
-    let onclick = {
+    let onreveal = {
         let put_email = props.email.clone();
         let email = email.clone();
         let visible = visible.clone();
@@ -103,31 +130,49 @@ pub fn EmailButton(props: &EmailButtonProps) -> Html {
         })
     };
 
+    let oncopy = {
+        let put_email = props.email.clone().to_string();
+        Callback::from(move |_| {
+            let clipboard = web_sys::window()
+                .expect("window")
+                .navigator()
+                .clipboard()
+                .unwrap();
+            // Todo: pop an info/error toast depending on result
+            _ = clipboard.write_text(&put_email);
+        })
+    };
+
     html! {
         if *visible {
             <div class={email_ctr_css}>
                 <input
-                    class={email_css}
+                    class={email_input_css}
                     value={email.to_string()}
                     readonly=true
                 />
                 <button
-                    class={copy_css}
+                    onclick={oncopy}
+                    class={copy_button_css}
                 >
-                    <Icon mask={IconMask::CopyToClipboard} />
+                    <Icon mask={IconMask::CopyToClipboard} data_aui_id={"copy"} />
                 </button>
             </div>
         } else {
             <div>
-                <button {onclick} class={btn_css}>
+                <button onclick={onreveal} class={btn_css}>
                     <span>
                         {"reveal email"}
                     </span>
                     <span
                         class={tooltip_css}
                         data-tooltip="This helps me fight bots!">
-                        <span style="position: absolute; left: 85px;">
-                            <Icon mask={IconMask::Question} fill={theme.background_color} />
+                        <span id="wrapper">
+                            <Icon
+                                mask={IconMask::Question}
+                                fill={theme.background_color}
+                                data_aui_id={"help"}
+                            />
                         </span>
                     </span>
                 </button>
