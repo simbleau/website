4335f49a18fc156623dedbe150f48a3ffbf93fa3
Spencer C. Imbleau <spencer@imbleau.com>
2023-10-31 23:08:26 -0400

    build: change dark theme link colors

--- src/style/themes/dark_theme.rs
+++ src/style/themes/dark_theme.rs
@@ -5,8 +5,8 @@ use once_cell::sync::Lazy;
 pub static DARK_THEME: Lazy<ThemeSpec> = Lazy::new(|| ThemeSpec {
     color: HexColor::from_u24(0xcccccc),
     background_color: HexColor::from_u24(0x222222),
-    link: HexColor::from_u24(0x66bbff),
-    link_hover: { lighten(&HexColor::from_u24(0x66bbff), 1.2) },
+    link: HexColor::from_u24(0x3399FF),
+    link_hover: { lighten(&HexColor::from_u24(0x3399FF), 1.2) },
     body_font: "Roboto",
     mono_font: "Roboto",
     fs: "1rem",
